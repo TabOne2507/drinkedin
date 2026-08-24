87e9041e672d7c23118220800cba92b86962991b
TabOne2507 <tabone2507@gmail.com>
2026-08-25 00:43:29 +0530

    fix private key newline handling for FIREBASE_SERVICE_ACCOUNT env var

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const PORT = process.env.PORT || 3000;
 let serviceAccount;
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  // Fix newlines in private key that get mangled by env var pasting
+  if (serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  }
 } else {
   serviceAccount = require('./serviceAccountKey.json');
 }
